b8b72febc667ca2acb4e8e8acfef89df59385ca2
Matt Davies <156399542+davo300@users.noreply.github.com>
2025-04-25 13:01:48 -0400

    Update SecretMessage.py

--- SecretMessage.py
+++ SecretMessage.py
@@ -3,7 +3,7 @@ Title: Decoding a Secret Message
 Author: Matt Davies
 Date: April 23, 2025
 
-This program untilizes requests and BeautifulSoup libraries to extract rows, cells, 
+This program utilizes requests and BeautifulSoup libraries to extract rows, cells, 
 and headers from a public document that has a table with symbols and associated x and y 
 coordinates for the given symbol. This program parses each symbol with its xy-coordinates 
 and then plots each symbol on a 2D-plane revealing a secret message.
